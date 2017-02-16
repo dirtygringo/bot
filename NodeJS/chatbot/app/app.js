d1f76b38727239f2d17a21d312d0df243776caea
Nenad Mihajlovic <nenad.llrnr@gmail.com>
2017-02-16 11:58:38 +0100

    Creating app.js file

---
+++ NodeJS/chatbot/app/app.js
@@ -0,0 +1,25 @@
+(function () {
+    'use strict';
+    var restify = require('restify'),
+        builder = require('botbuilder');
+
+    // Restify Server Setup
+    var server = restify.createServer();
+    server.listen(process.env.port || process.env.PORT || 3978, function () {
+        console.log('%s', server.name, server.url);
+    });
+
+    // Create chat bot
+    var connector = new builder.ChatConnector({
+        appId: process.env.MICROSOFT_APP_ID,
+        appPassword: process.env.MICROSOFT_APP_PASSWORD
+    });
+
+    var bot = new builder.UniversalBot(connector);
+    server.post('/api/messages', connector.listen());
+
+    bot.dialog('/', function (session) {
+        session.send('Hello world!');
+    });
+
+}());
