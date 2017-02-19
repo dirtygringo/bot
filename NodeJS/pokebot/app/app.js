ec83f99aad94d02301f077977e36539aae36d6b5
dirtygringo <nenad.llrnr@gmail.com>
2017-02-19 23:53:56 +0100

    ~

--- NodeJS/pokebot/app/app.js
+++ NodeJS/pokebot/app/app.js
@@ -2,7 +2,11 @@
     'use strict';
     var restify = require('restify'),
         builder = require('botbuilder'),
-        config = require('./config.js');
+        config = require('./config.js'),
+        recast = require('recastai');
+
+    // RecastAi client
+    var recastClient = new recast.Client(config.recast);
 
     // Creating connector
     var connector = new builder.ChatConnector({
@@ -13,13 +17,22 @@
     // Create chat bot
     var bot = new builder.UniversalBot(connector);
 
-    bot.dialog('/', function (session) {
-        session.send('Hello world!');
+    bot.dialog('/', (session) => {
+        recastClient.textRequest(session.message.text)
+            .then(res => {
+                let intent = res.intent();
+                let entity = res.get('pokemon');
+                session.send(`Intent: ${intent.slug}`);
+                session.send(`Entity: ${entity.name}`);
+            })
+            .catch(() => session.send('I need some sleep right now... Talk to me later'));
     });
 
     // Restify Server Setup
     var server = restify.createServer();
-    server.listen(8080);
+    server.listen(8080,() => {
+        console.log('Starting up server....');
+    });
 
     server.post('/', connector.listen());
 }());
--- NodeJS/pokebot/app/config.js
+++ NodeJS/pokebot/app/config.js
@@ -1,4 +1,5 @@
 const config = {
+    recast : '39d0f59bcf6ae4dc30f65962e9a4befc',
     appId: '0d09cdd0-6851-4f6f-95a4-63e7fd0917dc',
     appPassword: '7aPnudzJ0LUFGiAsX141w7a'
 };
